c2c4a39431e3d795de8c31e62069df4f4465a552
boruch d <dubywider@gmail.com>
2019-03-18 16:31:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,11 +1,11 @@
 def addition(num1, num2)
   
-  puts for=num1+ num2
-  return for
+  num1+ num2
+  
 end
 
 def subtraction(num1, num2)
-  puts num1-num2
+   num1-num2
 end
 
 def division(num1, num2)
@@ -17,11 +17,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
- puts num1%num2
+  num1%num2
 end
 
 def square_root(num)
-  puts Math.sqrt(num)
+   Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
